1e16d02053baf289f6091d463b43714ff48ddd83
Arpit Shah <arpitp1912@gmail.com>
2017-07-04 20:19:37 +0530

    Added customer message route

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,12 @@ var socket = io();
 socket.on('connect', function(){
     console.log("Connected to socket.io server!")
 })
+
+socket.on('message', function(message){
+    console.log(`New message:
+    ${message.text}`)
+})
+
+socket.emit('message', {
+    text: "Welcome to chat application!"
+    })
